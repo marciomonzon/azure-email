0070db3111b8753c4fd2d5f14952bd0217d6f8c2
monzondeveloper <monzondeveloper@gmail.com>
2024-09-07 15:42:52 -0300

    code to send email through azure comm service

---
+++ AzureEmail/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure;
+using Azure.Communication.Email;
+
+// This code retrieves your connection string from an environment variable.
+string connectionString = "COMMUNICATION_SERVICES_CONNECTION_STRING";
+var emailClient = new EmailClient(connectionString);
+
+
+EmailSendOperation emailSendOperation = emailClient.Send(
+    WaitUntil.Completed,
+    senderAddress: "DoNotReply@c7809194-168e-4f18-b421-fd337cb7789b.azurecomm.net",
+    recipientAddress: "myemail@emaildomain.com",
+    subject: "Test Email",
+    htmlContent: "<html><h1>Hello world via email.</h1l></html>",
+    plainTextContent: "Hello world via email.");
